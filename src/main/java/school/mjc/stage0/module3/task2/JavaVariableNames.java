f698ecdbdf4f8eaa9bcc8365d3bfffc8e7688468
NadejdaNick <nhohlova2021@gmail.com>
2023-08-28 14:50:42 +0300

    Changed

--- src/main/java/school/mjc/stage0/module3/task2/JavaVariableNames.java
+++ src/main/java/school/mjc/stage0/module3/task2/JavaVariableNames.java
@@ -2,13 +2,13 @@ package school.mjc.stage0.module3.task2;
 
 public class JavaVariableNames {
     public static void main(String[] args) {
-        int 2morrow = 0;
-        int $money = 10;
-        int %hundred = 100;
-        int !back = 1000;
-        int _underscore = 10000;
-        int #sharp = 100000;
-        int *star = 1000000;
-        int BADEXAMPLEOFVARNAME = 1000000;
+        var tomorrow = 0;
+        var $money = 10;
+        var hundred = 100;
+        var back = 1000;
+        var _underscore = 10000;
+        var sharp = 100000;
+        var star = 1000000;
+        var BADEXAMPLEOFVARNAME = 1000000;
     }
 }
